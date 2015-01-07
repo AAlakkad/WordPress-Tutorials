c33364421edd06f1f8db41e2f0d69564c43dd213
Ammar Alakkad <am.alakkad@gmail.com>
2015-01-07 17:34:27 +0200

    theme: use the loop inside index.php to display posts, and comment out the pagination tags

--- wp-content/themes/my_theme/index.php
+++ wp-content/themes/my_theme/index.php
@@ -1,50 +1,40 @@
 <?php
 get_header();
 ?>
-	<div class="main-content">
-		<div class="row">
-			<div class="large-8 right columns news">
-				<div class="row">
+<div class="main-content">
+	<div class="row">
+		<div class="large-8 right columns news">
+			<div class="row">
+				<?php
+				while(have_posts()) {
+					the_post();
+					?>
 					<div class="medium-12 columns">
 						<article class="card">
 							<header>
-								<h1>Post title</h1>
+								<h1><?php the_title();?></h1>
 							</header>
-							<div class="date">4 كانون الأول 2014</div>
+							<div class="date"><?php the_date();?></div>
 
-							<p>خلافاَ للاعتقاد <a href="#">السائد</a> فإن لوريم إيبسوم ليس نصاَ عشوائياً، بنذ العام 45 قبل الميلاد، مما يجعله أكثر من 2000 عام في القدم. قام البروفيسور "ريتشارد ماك لينتوك" (Richard McClintock) وهو بروفيسور اللغة اللاتينية في جامعة هامبدن-سيدني في فيرجينيا بالبحث عن أصول كلمة لاتينية غامضة في نص لوريم إيبسوم وهي "consectetur"، وخلال تتبعه لهذه الكلمة في الأدب اللاتيني اكتشف المصدر الغير قابل للشك. فلقد اتضح أن كلمات نص لوريم إيبسوم تأتي من الأقسام 1.10.32 و 1.10.33 من كتاب "حول أقاصي الخير والشر" (de Finibus Bonorum et Malorum) للمفكر شيشيرون (Cicero) والذي كتبه في عام 45 قبل الميلاد. هذا الكتاب هو بمثابة مقالة علمية مطولة في نظرية الأخلاق، وكان له شعبية كبيرة في عصر النهضة. السطر الأول من لوريم إيبسوم "Lorem ipsum dolor sit amet.." يأتي من سطر في القسم 1.20.32 من هذا الكتاب.</p>
+							<p><?php the_content();?></p>
 
 							<div class="read-more text-left">
-								<a href="#">تابع قراءة المقال &raquo;</a>
+								<a href="<?php the_permalink();?>">تابع قراءة المقال &raquo;</a>
 							</div>
 
 							<footer class="tags">
-								<span class="label">تقني</span>
-								<span class="label">تنمية</span>
-								<span class="label">منوعات</span>
+								<?php
+								$tags = get_tags();
+								foreach($tags as $tag) {
+									echo '<span class="label">' . $tag->name . '</span> ';
+								}
+								?>
 							</footer>
 						</article>
 					</div>
-					<div class="medium-12 columns">
-						<article class="card">
-							<header>
-								<h1>Post title</h1>
-							</header>
-							<div class="date">4 كانون الأول 2014</div>
-
-							<p>خلافاَ للاعتقاد <a href="#">السائد</a> فإن لوريم إيبسوم ليس نصاَ عشوائياً، بنذ العام 45 قبل الميلاد، مما يجعله أكثر من 2000 عام في القدم. قام البروفيسور "ريتشارد ماك لينتوك" (Richard McClintock) وهو بروفيسور اللغة اللاتينية في جامعة هامبدن-سيدني في فيرجينيا بالبحث عن أصول كلمة لاتينية غامضة في نص لوريم إيبسوم وهي "consectetur"، وخلال تتبعه لهذه الكلمة في الأدب اللاتيني اكتشف المصدر الغير قابل للشك. فلقد اتضح أن كلمات نص لوريم إيبسوم تأتي من الأقسام 1.10.32 و 1.10.33 من كتاب "حول أقاصي الخير والشر" (de Finibus Bonorum et Malorum) للمفكر شيشيرون (Cicero) والذي كتبه في عام 45 قبل الميلاد. هذا الكتاب هو بمثابة مقالة علمية مطولة في نظرية الأخلاق، وكان له شعبية كبيرة في عصر النهضة. السطر الأول من لوريم إيبسوم "Lorem ipsum dolor sit amet.." يأتي من سطر في القسم 1.20.32 من هذا الكتاب.</p>
-
-							<div class="read-more text-left">
-								<a href="#">تابع قراءة المقال &raquo;</a>
-							</div>
-
-							<footer class="tags">
-								<span class="label">تقني</span>
-								<span class="label">تنمية</span>
-							</footer>
-						</article>
-					</div>
+					<?php } ?>
 				</div>
+				<!--
 				<div class="row">
 					<div class="medium-12 columns">
 						<ul class="pagination">
@@ -60,12 +50,13 @@ get_header();
 						</ul>
 					</div>
 				</div>
-            </div>
-            <?php
-            get_sidebar();
-            ?>
+			-->
 		</div>
+		<?php
+		get_sidebar();
+		?>
 	</div>
+</div>
 <?php
 get_footer();
 ?>
