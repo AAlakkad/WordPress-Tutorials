b9c323e87ae16929b0a8a9117a19610222ff7082
Ammar Alakkad <am.alakkad@gmail.com>
2015-03-05 22:14:10 +0200

    Theme - Functions: enqueue custom style

--- wp-content/themes/my_theme/functions.php
+++ wp-content/themes/my_theme/functions.php
@@ -1,3 +1,8 @@
 <?php
 // Add theme thumbnail support, allowing each post to have thumbnail
 add_theme_support('post-thumbnails');
+
+// Enqueue custom style
+add_action('wp_enqueue_scripts', function() {
+    wp_enqueue_style('custom-pagination', get_template_directory_uri() . '/custom.css');
+});
