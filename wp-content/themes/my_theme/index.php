e0c832cadf5a7e6f84c1dc9177c3234659fda33d
Ammar Alakkad <am.alakkad@gmail.com>
2015-03-06 18:56:22 +0200

    Index: remove useless comment

--- wp-content/themes/my_theme/index.php
+++ wp-content/themes/my_theme/index.php
@@ -37,8 +37,6 @@ get_header();
 				<div class="row">
                     <div class="medium-12 columns">
                         <?php
-                            // add this style as custom style:
-                            // https://gist.github.com/AAlakkad/1f546f6c4a2975d9aab8
                             $args = array('type' => 'list');
                             echo paginate_links($args);
                         ?>
