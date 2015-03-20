ec02593401fc96df5d648ffc24ef3eff4ffd5a77
Ammar Alakkad <am.alakkad@gmail.com>
2015-03-20 17:22:16 +0200

    Functions: register all the theme's scripts/styles

--- wp-content/themes/my_theme/functions.php
+++ wp-content/themes/my_theme/functions.php
@@ -16,3 +16,11 @@ add_action('init', function() {
 add_action('widgets_init', function() {
     register_sidebar(['before_widget' => '<div class="card">', 'after_widget' => '</div>']);
 });
+
+add_action('wp_enqueue_scripts', function() {
+    wp_register_style('font-droid-arabic-kufi', 'http://fonts.googleapis.com/earlyaccess/droidarabickufi.css');
+    wp_register_style('main-style', get_template_directory_uri() . '/assets/css/main.css');
+
+    wp_register_script('modernizr', get_template_directory_uri(); . '/assets/js/modernizr.js');
+    wp_register_script('main-script', get_template_directory_uri(); . '/assets/js/vendor.js', ['modernizr'], false, true);
+});
